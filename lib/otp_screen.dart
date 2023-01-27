cdd26cbe12865182b08a406501a60c0b19b8b74c
pravithapravii <pravithasanker546@gmail.com>
2023-01-27 11:08:53 +0530

    demo1

--- lib/otp_screen.dart
+++ lib/otp_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:sms_autofill/sms_autofill.dart';
 
 import 'ScreenHome.dart';
@@ -48,70 +47,174 @@ class _VerifyOTPScreenState extends State<VerifyOTPScreen> with CodeAutoFill {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Verify Screen"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xfff7f6fb),
+      body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      "\n\n Otp sent to ${widget.phone}",
-                      style: TextStyle(color: Colors.black, fontSize: 26),
-                    ),
+          padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 32,
+                    color: Colors.black54,
                   ),
                 ),
-                Form(
-                  child: Center(
-                    child: PinFieldAutoFill(
-                      currentCode: codeValue,
-                      codeLength: 4,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        // LengthLimitingTextInputFormatter(1),
-                        FilteringTextInputFormatter.digitsOnly
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple.shade50,
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset(
+                  'assets/images/9dcf525c-0f01-4f62-bb75-02381a22a4b2.jpg',
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              const Text(
+                'Verification',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Enter your OTP code number",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 28,
+              ),
+              Container(
+                padding: EdgeInsets.all(28),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _textFieldOTP(first: true, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: true),
                       ],
-                      onCodeChanged: (code) {
-                        print("onCodeChanged $code");
-                        setState(() {
-                          codeValue = code.toString();
-                        });
-                      },
-                      onCodeSubmitted: (val) {
-                        print("onCodeSubmitted $val");
-                      },
                     ),
-                  ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => ScreenHome()));
+                        },
+                        style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.purple),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(14.0),
+                          child: Text(
+                            'Verify',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                const SizedBox(
-                  height: 20,
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Text(
+                "Didn't you receive any code?",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
                 ),
-                Center(
-                  child: ElevatedButton(
-                      onPressed: () {
-                        print(codeValue);
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: ((context) => ScreenHome())));
-                      },
-                      child: const Text("Verify OTP")),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Text(
+                "Resend New Code",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                    child: ElevatedButton(
-                        onPressed: listenOtp, child: const Text("Resend")))
-              ],
-            ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldOTP({required bool first, last}) {
+    return Container(
+      height: 85,
+      child: AspectRatio(
+        aspectRatio: 0.70,
+        child: TextField(
+          autofocus: true,
+          onChanged: (value) {
+            if (value.length == 1 && last == false) {
+              FocusScope.of(context).nextFocus();
+            }
+            if (value.length == 0 && first == false) {
+              FocusScope.of(context).previousFocus();
+            }
+          },
+          showCursor: false,
+          readOnly: false,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          decoration: InputDecoration(
+            counter: Offstage(),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: Colors.black12),
+                borderRadius: BorderRadius.circular(12)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: Colors.purple),
+                borderRadius: BorderRadius.circular(12)),
           ),
         ),
       ),
@@ -194,10 +297,10 @@ class _VerifyOTPScreenState extends State<VerifyOTPScreen> with CodeAutoFill {
 //             //         child: TextFormField(
 //             //           textAlign: TextAlign.center,
 //             //           keyboardType: TextInputType.number,
-                      // inputFormatters: [
-                      //   LengthLimitingTextInputFormatter(1),
-                      //   FilteringTextInputFormatter.digitsOnly
-                      // ],
+//             //           inputFormatters: [
+//             //             LengthLimitingTextInputFormatter(1),
+//             //             FilteringTextInputFormatter.digitsOnly
+//             //           ],
 //             //           style: TextStyle(color: Colors.black, fontSize: 30),
 //             //           decoration:
 //             //               InputDecoration(filled: true, fillColor: Colors.grey),
